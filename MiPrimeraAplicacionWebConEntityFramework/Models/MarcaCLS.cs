501c87d746e1163a21897161a7afca3cac76985a
JOSE MOJICA <JOMOJICA@cgr.gob.pa>
2022-04-21 16:18:03 -0500

    Creando el primer modelo

---
+++ MiPrimeraAplicacionWebConEntityFramework/Models/MarcaCLS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimeraAplicacionWebConEntityFramework.Models
+{
+    public class MarcaCLS
+    {
+        [Display(Name ="ID Marca")]
+        public int IIDMARCA { get; set; }
+        [Display(Name = "Nombre")]
+        public string NOMBRE { get; set; }
+        [Display(Name = "Descripcion")]
+        public string DESCRIPCION { get; set; }
+        public Nullable<int> BHABILITADO { get; set; }
+
+    }
+}
