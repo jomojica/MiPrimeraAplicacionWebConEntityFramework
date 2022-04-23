104565f3a0763d438eb0291ed56103edf676b34b
Jose <alforhanz@gmail.com>
2022-04-23 13:06:01 -0500

    subiendo los archivos de MiPrimeraAplicacionWEbConEntityFramework

---
+++ MiPrimeraAplicacionWEbConEntityFramework/Controllers/MarcaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MiPrimeraAplicacionWEbConEntityFramework.Models;
+
+namespace MiPrimeraAplicacionWEbConEntityFramework.Controllers
+{
+    public class MarcaController : Controller
+    {
+        // GET: Marca
+        public ActionResult Index()
+        {
+            using (var bd = new BDPasajeEntities())
+            {
+
+            }
+
+                return View();
+        }
+    }
+}
---
+++ MiPrimeraAplicacionWEbConEntityFramework/Models/MarcaCLS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimeraAplicacionWEbConEntityFramework.Models
+{
+    public class MarcaCLS
+    {
+        [Display(Name ="Id Marca")]
+        public int iidmarca { get; set; }
+        [Display(Name = "Nombre")]
+        public int nombre { get; set; }
+        [Display(Name = "Descripción")]
+        public int descripcion{ get; set; }
+        //[Display(Name = "B-Habilitado")]
+        public int bhabilitado { get; set; }
+    }
+}
